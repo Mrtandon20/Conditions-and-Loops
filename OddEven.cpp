e3978908bf965c83dfae928206266792beaf50c2
Varun Tandon <75672315+Mrtandon20@users.noreply.github.com>
2022-04-12 15:38:04 +0530

    A Number is Even or Odd ?

---
+++ OddEven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the value of Number : ";
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"Even No"<<endl;
+    }else{
+        cout<<"Odd No"<<endl;
+    }
+}
